90ee0cddfa0a7be7f284b114970f40bc186a832a
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-05 19:15:47 -0500

    Serves files yay

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,13 @@
+const path       = require("path");
+
 module.exports = function(app){
     
     app.get(`/`,(req,res)=>{
-        res.send(path.join(__dirname + `/../public/home.html`));
+        res.sendFile(path.join(__dirname + `/../../public/home.html`));
     });
 
     app.get(`/reservations`,(req,res)=>{
-        res.send(path.join(__dirname + `/../public/tables.html`));
+        res.sendFile(path.join(__dirname + `/../../public/tables.html`));
     });
 
 }
