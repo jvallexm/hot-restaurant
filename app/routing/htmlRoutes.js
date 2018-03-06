2d49497f4fb805708ad4abc800fe20e58e5d2fea
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-05 19:08:32 -0500

    Added serving files

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
     
     app.get(`/`,(req,res)=>{
-        res.send("home");
+        res.send(path.join(__dirname + `/../public/home.html`));
     });
 
     app.get(`/reservations`,(req,res)=>{
-        res.send("res");
+        res.send(path.join(__dirname + `/../public/tables.html`));
     });
 
 }
